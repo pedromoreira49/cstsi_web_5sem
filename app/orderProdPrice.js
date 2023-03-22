d7a9401133ea4b8aaeef95f51f892155472712bc
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-22 16:37:29 -0300

    feat: adicionado arquivo para ordenar dados pelo Preço

--- app/orderProdPrice.js
+++ app/orderProdPrice.js
@@ -1,8 +1,11 @@
 import  * as fb from "firebase/database";// sera preciso usar fb antes de cada funcao
-import { dbConnect } from "./connetToFB.js";
+import { dbConnect } from "./connectToFB.js";
 
 dbConnect()
-.then(db=>{//db contem a referencia ao banco
-    console.log(db)//mostra informacoes da conexao(pode excluir)
-    //implemente aqui
+.then(db=>{
+    const refDb = fb.ref(db, 'produtos')
+    const query = fb.query(refDb, fb.orderByChild('preco'))
+    fb.onChildAdded(query, datas => {
+        console.log(datas.val())
+    })
 }).catch(err=>console.log(err))
