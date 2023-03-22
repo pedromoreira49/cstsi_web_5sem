34d81d7ad03d081de74d4e965de6bd97d79ec69b
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-22 16:37:49 -0300

    feat: adicionado arquivo para visualizar todos os dados

--- app/readAllData.js
+++ app/readAllData.js
@@ -1,8 +1,18 @@
 import  * as fb from "firebase/database";// sera preciso usar fb antes de cada funcao
-import { dbConnect } from "./connetToFB.js";
+import { dbConnect } from "./connectToFB.js";
 
 dbConnect()
-.then(db=>{//db contem a referencia ao banco
-    console.log(db)//mostra informacoes da conexao(pode excluir)
-    //implemente aqui
+.then(db=>{
+    const refDb = fb.ref(db)
+    fb.get(fb.child(refDb, `produtos/`)).then((data) => {
+        if(data.exists()){
+            console.table(data.val())
+        }else{
+            console.log("Dados não encontrados")
+        }
+    }).catch(err => {
+        console.error(err.message)
+    }).finally(() => {
+        process.exit()
+    })
 }).catch(err=>console.log(err))
